532e46adaccb158f3595c28e7dd9f5edfa8716c8
pcheung <pcheung@NB00153>
2020-11-23 21:02:51 +0800

    commit again...

--- src/com/git/Test1.java
+++ src/com/git/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String hungry = "yes";
+		String hungry = "no";
 		if(hungry.equals("yes")) {
 			System.out.println("eat something...");
 		} else {
