f7a880eb195082d7d2a10af1dba72f60bbeb1b91
pcheung <pcheung@NB00153>
2020-11-23 21:00:43 +0800

    first commit

---
+++ src/com/git/Test1.java
@@ -0,0 +1,16 @@
+package com.git;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry = "yes";
+		if(hungry.equals("yes")) {
+			System.out.println("eat something...");
+		} else {
+			System.out.println("do work...");
+		}
+
+	}
+
+}
